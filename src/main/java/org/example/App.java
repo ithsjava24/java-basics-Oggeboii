92c2dc7e0017255eadad4836feddcb9c58cd9d6d
0skar <0skar01@hotmail.se>
2024-09-14 10:04:42 +0200

    Removed unnecessary loop and if statements, corrected variable names

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -57,6 +57,7 @@ public class App {
                 }
             }
         }
+
         public void Inmatning() {
             for (int i = 0; i < inData.length; i++) {
                 try {
@@ -68,132 +69,128 @@ public class App {
                 }
             }
         }
+
         public void minMaxMiddle() {
             int[] minMaxPriceArray = inData;
             int sum = 0;
             float averagePrice;
             int highestPrice = 0;
-            int highestPricePosition = 0;
-            int lowestPricePosition = 0;
+            int highestPriceHour = 0;
+            int lowestPriceHour = 0;
             int lowestPrice = Integer.MAX_VALUE;
             for (int i = 0; i <minMaxPriceArray.length; i++) {
                 if (minMaxPriceArray[i] > highestPrice) {
                     highestPrice = minMaxPriceArray[i];
-                    highestPricePosition = i;
+                    highestPriceHour = i;
                 }
                 if (minMaxPriceArray[i] < lowestPrice) {
                     lowestPrice = minMaxPriceArray[i];
-                    lowestPricePosition = i;
+                    lowestPriceHour = i;
                 }
                 sum = minMaxPriceArray[i] + sum;
             }
-
             averagePrice = ((float) sum /minMaxPriceArray.length);
-            System.out.printf("Lägsta pris: %02d-%02d, %d öre/kWh\n" , lowestPricePosition, (lowestPricePosition + 1), lowestPrice);
-            System.out.printf("Högsta pris: %02d-%02d, %d öre/kWh\n" , highestPricePosition, (highestPricePosition+1), highestPrice);
+            System.out.printf("Lägsta pris: %02d-%02d, %d öre/kWh\n" , lowestPriceHour, (lowestPriceHour + 1), lowestPrice);
+            System.out.printf("Högsta pris: %02d-%02d, %d öre/kWh\n" , highestPriceHour, (highestPriceHour+1), highestPrice);
             System.out.printf("Medelpris: " + "%.2f öre/kWh\n", averagePrice);
         }
+
         public void sort(){
-            int[] sortArray  = new int[inData.length];
+            int[] valueArray  = new int[inData.length];
             for (int i = 0; i < inData.length; i++) {
-                sortArray[i] = inData[i];
+                valueArray[i] = inData[i];
             }
-            int[]positionArray = new int[24];
+            int[]hourArray = new int[24];
             for (int i = 0; i < 24; i++) {
-                positionArray[i] = i;
+                hourArray[i] = i;
             }
             int tempValue;
             int tempPosition;
             boolean swapped;
-            for (int i = 0; i < sortArray.length-1; i++) {
+            for (int i = 0; i < valueArray.length-1; i++) {
                 swapped = false;
-                for (int j = 0; j < sortArray.length-1; j++) {
-                    if (sortArray[j] < sortArray[j+1]) {
-                        tempValue = sortArray[j];
-                        sortArray[j] = sortArray[j+1];
-                        sortArray[j+1] = tempValue;
-                        tempPosition = positionArray[j];
-                        positionArray[j] = positionArray[j+1];
-                        positionArray[j+1] = tempPosition;
+                for (int j = 0; j < valueArray.length-1; j++) {
+                    if (valueArray[j] < valueArray[j+1]) {
+                        tempValue = valueArray[j];
+                        valueArray[j] = valueArray[j+1];
+                        valueArray[j+1] = tempValue;
+                        tempPosition = hourArray[j];
+                        hourArray[j] = hourArray[j+1];
+                        hourArray[j+1] = tempPosition;
                         swapped = true;
                     }
                 }
                 if (!swapped) {break;}
             }
             for (int i = 0; i <inData.length ; i++) {
-                System.out.printf("%02d-%02d %d öre\n" , positionArray[i], (positionArray[i] + 1), sortArray[i]);
+                System.out.printf("%02d-%02d %d öre\n" , hourArray[i], (hourArray[i] + 1), valueArray[i]);
             }
         }
         public void cheapest(){
             float cheapestFour = Integer.MAX_VALUE;
             int cheapestFirst = 0;
-            for (int i = 0; i < inData.length-4; i++) {
+            for (int i = 0; i < inData.length-3; i++) {
                 if (cheapestFour > inData[i] + inData[i+1] + inData[i+2] + inData[i+3]){
                     cheapestFour = inData[i] + inData[i+1] + inData[i+2] + inData[i+3];
                     cheapestFirst = i;
                 }
             }
             System.out.printf("Påbörja laddning klockan %02d\n", cheapestFirst);
-            System.out.printf( "Medelpris 4h: %.1f öre/kWh\n",cheapestFour/4);
+            System.out.printf("Medelpris 4h: %.1f öre/kWh\n",cheapestFour/4);
         }
         public void visual(){
             int lowestPrice = Integer.MAX_VALUE;
-            for (int inDatum : inData) {
-                if (lowestPrice > inDatum) {
-                    lowestPrice = inDatum;
+            for (int inDataValue : inData) {
+                if (lowestPrice > inDataValue) {
+                    lowestPrice = inDataValue;
                 }
             }
-            int sixth =530;
-            int fifth = 432;
-            int fourth = 334;
-            int third =  236;
-            int second = 138;
-            int first =40;
+            int seventh =530;
+            int sixth = 432;
+            int fifth = 334;
+            int fourth =  236;
+            int third = 138;
+            int second =40;
             if (lowestPrice < 0) {
-                sixth = 100;
-                fifth = 77;
-                fourth = 55;
-                third = 32;
-                second = 10;
-                first = -12;
+                seventh = 100;
+                sixth = 77;
+                fifth = 55;
+                fourth = 32;
+                third = 10;
+                second = -12;
             }
-        System.out.print(sixth +"|");
-            for (int i = 7; i > 0; i--) {
-                if (i >1) {
-                    for (int inDatum : inData) {
-                        String blank = " ";
-                        if (inDatum >= first && i == 2) {
-                            System.out.print(blank + " x");
-                        } else if (inDatum >= second && i == 3) {
-                            System.out.print(blank + " x");
-                        } else if (inDatum >= third && i == 4) {
-                            System.out.print(blank + " x");
-                        } else if (inDatum >= fourth && i == 5) {
-                            System.out.print(blank + " x");
-                        } else if (inDatum >= fifth && i == 6) {
-                            System.out.print(blank + " x");
-                        } else if (inDatum >= sixth && i == 7) {
-                            System.out.print(blank + " x");
-                        } else {
-                            System.out.print(blank + "  ");
-                        }
+        System.out.print(seventh +"|");
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < inData.length; j++) {
+                    String blank = " ";
+                    if (i == 0 ) {
+                        System.out.printf(" %02d", j);
+                    } else if (i == 1) {
+                        System.out.print("---");
+                    } else if (inData[j] >= second && i == 2) {
+                        System.out.print(blank + " x");
+                    } else if (inData[j] >= third && i == 3) {
+                        System.out.print(blank + " x");
+                    } else if (inData[j] >= fourth && i == 4) {
+                        System.out.print(blank + " x");
+                    } else if (inData[j] >= fifth && i == 5) {
+                        System.out.print(blank + " x");
+                    } else if (inData[j] >= sixth && i == 6) {
+                        System.out.print(blank + " x");
+                    } else if (inData[j] >= seventh && i == 7) {
+                        System.out.print(blank + " x");
+                    } else {
+                        System.out.print(blank + "  ");
                     }
                 }
                 if(i == 3) {
-                    if (first<0)
-                        System.out.print("\n"+first+"|");
-                    else
-                        System.out.print("\n "+first+"|");
-                }
-                else
+                    if (second<0) {
+                        System.out.print("\n"+second+"|");
+                    } else {
+                        System.out.print("\n "+second+"|");
+                    }
+                } else if (i > 0) {
                     System.out.print("\n   |");
-                for (int j = 0; j < inData.length; j++) {
-                    if (i == 2) {
-                        System.out.print("---");
-                    }
-                    if(i == 1) {
-                        System.out.printf(" %02d", j);
-                    }
                 }
             }
             System.out.print("\n");
